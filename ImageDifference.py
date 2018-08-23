d608f67edcd3a08c4cc0c9029dab1e988a9028f6
Alex Reyes <alexreyes@Alexs-MacBook-Pro.local>
2018-08-23 00:56:51 -0500

    After Being Unable to Solve The Angular Axis Issue With Ellipses, I Switched From Ellipse Bounding to Rectangular Bounding Which Actually Resulted In Tighter Bounding Boxes.

--- ImageDifference.py
+++ ImageDifference.py
@@ -35,8 +35,11 @@ kmeans.fit(points)
 # and estimate an elipse that covers the 
 # cluster
 for i in range(nClusters):
+	# get edge points
 	left = None
 	right = None
+	up = None
+	down = None
 	for j in range(len(points)):
 		if kmeans.labels_[j] == i:
 			# point is left of center
@@ -54,44 +57,87 @@ for i in range(nClusters):
 					if points[j][0] > right[0]:
 						right = points[j]
 
-	# get x radius of ellipse
-	dx = abs(left[0] - right[0])
-	dy = abs(left[1] - right[1])
-	rx = math.sqrt(math.pow(dx, 2) + math.pow(dy, 2)) - 30
+			# point is up of center
+			if points[j][1] < kmeans.cluster_centers_[i][1]:
+				if up is None:
+					up = points[j]
+				else:
+					if points[j][1] < up[1]:
+						up = points[j]
+			# point is down of center
+			elif points[j][1] > kmeans.cluster_centers_[i][1]:
+				if down is None:
+					down = points[j]
+				else:
+					if points[j][1] > down[1]:
+						down = points[j]
 
-	# get angle between left and right points in degress
-	angle = math.degrees(math.atan2(dy, dx))
+	tl = (left[0], up[1])
+	tr = (right[0], up[1])
+	bl = (left[0], down[1])
+	br = (right[0], down[1])
+
+	cv2.line(imgDif, (math.floor(tl[0]), math.floor(tl[1])),
+		(math.floor(tr[0]), math.floor(tr[1])), (255, 255, 255), 4)
+
+	cv2.line(imgDif, (math.floor(tr[0]), math.floor(tr[1])),
+		(math.floor(br[0]), math.floor(br[1])), (255, 255, 255), 4)
+
+	cv2.line(imgDif, (math.floor(br[0]), math.floor(br[1])),
+		(math.floor(bl[0]), math.floor(bl[1])), (255, 255, 255), 4)
+
+	cv2.line(imgDif, (math.floor(bl[0]), math.floor(bl[1])),
+		(math.floor(tl[0]), math.floor(tl[1])), (255, 255, 255), 4)
+
+	# if right is higher than left, average
+	# with up point, recalculate
+	#if right[1] < left[1]:
+	#	right = ((right[0] + up[0]) / 2, (right[1] + up[1]) / 2)
+	#	left = ((left[0] + down[0]) / 2, (left[1] + down[1]) / 2)
+
+	# if left is higher than right, average
+	# with down point, recalculate
+	#elif right[1] > left[1]:
+	#	right = ((right[0] + down[0]) / 2, (right[1] + down[1]) / 2)
+	#	left = ((left[0] + up[0]) / 2, (left[1] + up[1]) / 2)
+
+	# get x radius of ellipse
+	#dx = abs(left[0] - right[0])
+	#dy = abs(left[1] - right[1])
+	#rx = math.sqrt(math.pow(dx, 2) + math.pow(dy, 2)) - 20
+
+	# get angle between left and right
+	# points in degress
+	#angle = math.degrees(math.atan2(dy, dx))
 
 	# angle adjustments
-	if right[1] < left[1]:
-		angle += 135
+	#if right[1] < left[1]:
+	#	angle -= 45
 
 	# get ry
-	# iterate through every point, and compare its
-	# angle in relation to the center point. Find the 
-	# two furthest points on the line that is
-	# perpendicular to the angle created from the left
-	# and right points. Measure this distance to get ry.
-	for j in range(len(points)):
-		if kmeans.labels_[j] == i:
-			tx = points[j][0] # point x
-			ty = points[j][1] # point y
-			cx = kmeans.cluster_centers_[i][0] # center x
-			cy = kmeans.cluster_centers_[i][1] # center y
-			dx = abs(tx - cx) # distance between x coordinates
-			dy = abs(ty - cy) # distance between y coordinates
-			an = math.floor(math.degrees(math.atan2(dy, dx))) # angle
-			
-			perpAn = math.floor(math.floor(angle) - an)
-			print(str(perpAn) + "\n" + str(math.floor(angle)) + "\n")
-			if perpAn % 90 <= 10: #and abs(perpAn - angle) > 10:
-				cv2.line(imgDif, (math.floor(tx), math.floor(ty)), (math.floor(cx), math.floor(cy)), (255, 255, 255), 1)
+	# find all points on the verticle axis
+	# and find the distance between the
+	# edges and then tilt that distance 
+	# by a complementary angle to the theta
+	#dy = 0
+	#p = None
+	#for j in range(len(points)):
+	#	if kmeans.labels_[j] == i:
+	#		if math.floor(points[j][0]) == math.floor(kmeans.cluster_centers_[i][0]): 
+	#			if (abs(kmeans.cluster_centers_[i][1] - points[j][1]) > dy):
+	#				dy = abs(kmeans.cluster_centers_[i][1] - points[j][1])
+	#				p = points[j]
 
-	ry = 40;
+	#theta = angle
+	#ry = (dy / math.cos(theta))
+	#ry = 30
 	
+	#cv2.line(imgDif, (math.floor(right[0]), math.floor(right[1])),
+	#	(math.floor(left[0]), math.floor(left[1])), (255, 255, 255), 4)
+
 	# draw ellipse
-	cv2.ellipse(imgDif, (math.floor(kmeans.cluster_centers_[i][0]), math.floor(kmeans.cluster_centers_[i][1])),
-		(math.floor(rx), math.floor(ry)), angle, 0, 360, (255, 255, 255), 2)
+	#cv2.ellipse(imgDif, (math.floor(kmeans.cluster_centers_[i][0]), math.floor(kmeans.cluster_centers_[i][1])),
+	#	(math.floor(abs(rx)), math.floor(abs(ry))), angle, 0, 360, (255, 255, 255), 2)
 
 # output JSON data
 # check for solutions JSON file to append
